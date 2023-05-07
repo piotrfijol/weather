3bc9627e51d13cc1e72e19421359621ea86651b5
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-07 18:52:50 +0200

    Add search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { SearchInput } from './components/SearchInput'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('');
 
   return (
     <>
+      <form>
+        <SearchInput 
+          onChange={(e) => {
+            setSearch(e.currentTarget.value)
+          }}
+          value={search} 
+        />
+      </form>
     </>
   )
 }
