6031222b0e61d58728925e7419ae024afb3f4b8f
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-08 15:42:01 +0200

    Improve responsivness of SearchInput

--- src/components/icons/NavigationIcon.tsx
+++ src/components/icons/NavigationIcon.tsx
@@ -1,7 +1,7 @@
 
 export const NavigationIcon = () => {
   return (
-    <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <svg viewBox="0 0 13 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <path d="M13 5c-.8-3.5-3.8-5-6.4-5A6.4 6.4 0 0 0 .2 5c-.9 3.8 1.5 7 3.6 9.1a4 4 0 0 0 5.6 0c2.1-2 4.5-5.3 3.6-9.1ZM6.6 8.7a2.3 2.3 0 1 1 0-4.7 2.3 2.3 0 0 1 0 4.7Z" />
     </svg>
   )
