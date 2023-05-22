b207086f47de01f6bca64668e3c5cebb379c88a5
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-22 08:39:03 +0200

    Style main CurrentDayWeather info

--- src/components/CurrentDayWeather/index.tsx
+++ src/components/CurrentDayWeather/index.tsx
@@ -9,20 +9,24 @@ export const CurrentDayWeather = () => {
 
     return (
         <section className="weather-main">
-            <h2 className="weather-main__city"><span className="country-code">[PL]</span> Kielce</h2>
-            <div className="weather-main__timers">
-                <Time is="sunrise" at={100000000} format={12}/>
-                <Time is="sunset" at={1} format={12}/>
-            </div>
-            <div className="container">
-                <div className="weather-main__illustration">
-                    <img src="http://openweathermap.org/img/wn/04d@4x.png" alt="" />
+            <div className="wrapper">
+                <h2 className="weather-main__city"><span className="country-code">[PL]</span> Kielce</h2>
+                <div className="weather-main__timers">
+                    <Time is="sunrise" at={100000000} format={12}/>
+                    <Time is="sunset" at={1} format={12}/>
                 </div>
-                <div className="weather-main__info">
-                    <Temperature temp={24} />
-                    <InfoEntry icon={HumidityIcon} 
-                        info={"42%"} alt="water drop with the percentage sign on it" />
-                    <InfoEntry icon={BarometerIcon} info={"1024hPa"} alt="barometer"/>
+                <div className="container">
+                    <div className="weather-main__illustration">
+                        <img src="http://openweathermap.org/img/wn/04d@4x.png" alt="" />
+                    </div>
+                    <div className="weather-main__info">
+                        <Temperature temp={16} className="weather-main__temperature"/>
+                        <div className="container">
+                            <InfoEntry icon={HumidityIcon} 
+                                info={"42%"} alt="water drop with the percentage sign on it" />
+                            <InfoEntry icon={BarometerIcon} info={"1024hPa"} alt="barometer"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
