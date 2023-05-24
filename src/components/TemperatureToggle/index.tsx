3ef78fd2ebf85f1b3d98155a78814bc8b0310122
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-24 11:22:38 +0200

    Describe the form for selecting temperature unit

--- src/components/TemperatureToggle/index.tsx
+++ src/components/TemperatureToggle/index.tsx
@@ -18,9 +18,12 @@ export const TemperatureToggle = ({selectedUnit = "celsius", onToggle}: Temperat
   return (
     <div className="temperature-toggle">
       <form>
-        <TempUnitControl unit="celsius" symbol="C" checked={selectedUnit === "celsius"} onChange={handleChange}/>
-        <div className="temperature-toggle__delimiter" />
-        <TempUnitControl unit="fahrenheit" symbol="F" checked={selectedUnit === "fahrenheit"} onChange={handleChange}/>
+        <fieldset>
+          <legend>Select your temperature unit</legend>
+          <TempUnitControl unit="celsius" symbol="C" checked={selectedUnit === "celsius"} onChange={handleChange}/>
+          <div className="temperature-toggle__delimiter" />
+          <TempUnitControl unit="fahrenheit" symbol="F" checked={selectedUnit === "fahrenheit"} onChange={handleChange}/>
+        </fieldset>
       </form>
     </div>
   )
