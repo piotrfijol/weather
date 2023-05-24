b1d5180b044598f8e49a8b2e560277e25d368925
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-24 11:26:53 +0200

    Replace icons for wind information

--- src/components/CurrentDayWeather/index.tsx
+++ src/components/CurrentDayWeather/index.tsx
@@ -3,6 +3,8 @@ import { Time } from "../Time";
 import { Temperature } from "../Temperature";
 import HumidityIcon from "@assets/humidity.svg";
 import BarometerIcon from "@assets/barometer.svg";
+import SpeedometerIcon from "@assets/Speedometer.svg";
+import DirectionIcon from "@assets/direction.svg";
 import WindIcon from "@assets/wind.svg";
 import "./CurrentDayWeather.scss";
 
@@ -25,23 +27,23 @@ export const CurrentDayWeather = () => {
                             <Temperature temp={16} className="today__temperature"/>
                             <div className="today__info">
                                 <InfoEntry icon={HumidityIcon} 
-                                    info={"42%"} alt="water drop with the percentage sign on it" />
-                                <InfoEntry icon={BarometerIcon} info={"1024hPa"} alt="barometer"/>
+                                    info={"42%"} alt="humidity" />
+                                <InfoEntry icon={BarometerIcon} info={"1024hPa"} alt="atmospheric pressure"/>
                             </div>
                         </div>
                     </div>
                 </div>
                 <section className="wind">
                     <div className="wind__image">
-                    <img src={WindIcon} alt="" />
+                    <img src={WindIcon} alt="wind" />
                     </div>
                     <div className="wind__data">
                         <InfoEntry 
-                            icon={<img src={HumidityIcon} style={{transform: `rotate(${10}deg)`}}/>} 
+                            icon={<img src={DirectionIcon} alt="direction" style={{transform: `rotate(${44}deg)`}}/>} 
                             alt="direction" 
                             info={"North West"}
                         />
-                        <InfoEntry icon={HumidityIcon} alt="speed" info={"52 km/h"}/>
+                        <InfoEntry icon={SpeedometerIcon} alt="speed" info={"52 km/h"}/>
                     </div>
                 </section>
             </div>
