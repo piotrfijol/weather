a4ff7cda2e91221a278b3bb6501f64fe3f992de0
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-25 23:44:08 +0200

    Set global query config

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import './App.css';
 import { SkeletonTheme } from "react-loading-skeleton";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({defaultOptions: {queries: {retry: 1, refetchOnWindowFocus: false}}})
 
 function App() {
   const [temperatureUnits, setTemperatureUnits] = useState<TemperatureUnitsInfo>(defaultTempUnitsInfo);
