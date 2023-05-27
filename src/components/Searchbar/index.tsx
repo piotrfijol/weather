1aee736a9f562ef2abb7f3cb37e29e596d6dca2c
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-27 12:28:34 +0200

    Use a handler when autocomplete option gets selected

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -3,6 +3,7 @@ import { SearchLocation } from '../../types/location';
 import { SearchInput } from '../SearchInput';
 import { useState, useEffect } from "react";
 import { fetchAutocompletion } from '../../fetch/autocompletion';
+import { AutcompleteSuggestion } from '../../types/location';
 
 interface SearchbarProps {
     onLocationChange: (location: SearchLocation) => void
@@ -11,12 +12,12 @@ interface SearchbarProps {
 
 export const Searchbar = ({ onLocationChange }: SearchbarProps) => {
     const [search, setSearch] = useState<string>('');
-    const { isLoading, error, data, refetch } = useQuery({
+    const [location, setLocation] = useState<SearchLocation>("Warsaw");
+    const { data, refetch } = useQuery({
         queryKey: ['city-autocompletion', search],
         queryFn: () => fetchAutocompletion(search),
         enabled: false
     });
-    const [location, setLocation] = useState<SearchLocation>("Warsaw");
 
     const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -30,7 +31,7 @@ export const Searchbar = ({ onLocationChange }: SearchbarProps) => {
         let timeoutID = setTimeout(() => {
             if (search.trim() !== '')
                 refetch()
-        }, 500);
+        }, 300);
 
         return () => {
             clearTimeout(timeoutID);
@@ -40,6 +41,10 @@ export const Searchbar = ({ onLocationChange }: SearchbarProps) => {
     const handleOnLocation = (position: GeolocationPosition) => {
         setLocation(position);
     };
+
+    const handleOnAutocomplete = (location: AutcompleteSuggestion) => {
+        setLocation(location);
+    }
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.currentTarget.value)
@@ -54,7 +59,9 @@ export const Searchbar = ({ onLocationChange }: SearchbarProps) => {
             <SearchInput
                 onChange={handleInputChange}
                 onLocation={handleOnLocation}
+                onAutocomplete={handleOnAutocomplete}
                 value={search}
+                autocomplete={data}
             />
         </form>
     )
