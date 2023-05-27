4be220c4bf8000095cd7db3656d3499150fbee96
Piotr Fijol <piotr.fijol1@gmail.com>
2023-05-27 12:19:40 +0200

    Replace placeholder content with props data

--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,16 +1,32 @@
+import { AutcompleteSuggestion } from "../../types/location";
 import "./Autocomplete.scss";
 
-export const Autocomplete = ({className = ""}) => {
-  return (
-    <div className={`autocomplete ${className}`}>
-        <div className="autocomplete__suggestion">
-        <span className="autocomplete__city">Kielce</span>
-        <span className="autocomplete__location-details">Poland, Świętokrzyskie</span>
-        </div>
-        <div className="autocomplete__suggestion">
-        <span className="autocomplete__city">Kielce</span>
-        <span className="autocomplete__location-details">Poland, Świętokrzyskie</span>
-        </div>
+interface AutocompleteProps {
+    className: string,
+    data: AutcompleteSuggestion[],
+    onAutocomplete: (location: AutcompleteSuggestion) => void
+} 
+
+export const Autocomplete = ({className = "", data = [], onAutocomplete}: AutocompleteProps) => {
+
+    const handleSelect =(selected: AutcompleteSuggestion) => {
+        onAutocomplete(selected);
+    };
+
+    return (
+    <div className={`autocomplete ${className}`} tabIndex={0}>
+        {
+            data.map((suggestion) => (
+                <div 
+                    className="autocomplete__suggestion" 
+                    key={suggestion.id} 
+                    onClick={(e) => {handleSelect(suggestion)}}
+                >
+                    <span className="autocomplete__city">{suggestion.city}</span>
+                    <span className="autocomplete__location-details">{suggestion.country} {suggestion.state}</span>
+                </div>
+            ))
+        }
     </div>
   )
 }
